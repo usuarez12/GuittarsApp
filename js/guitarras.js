c736ae22fbf5b1fcf8d174bce5c002a9238b7471
uriel <urielgonzalez89pl@gmail.com>
2025-10-13 11:19:10 -0600

    01 load guitars

---
+++ js/guitarras.js
@@ -0,0 +1,10 @@
+export const db = [
+    {
+        id: 1,
+        nombre: 'Lukather',
+        imagen: 'guitarra_01',
+        descripcion: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        precio: 299,
+    },
+
+]
