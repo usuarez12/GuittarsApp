d0772c8344ee4f9f17e949cf4ad185ae2118e461
uriel <urielgonzalez89pl@gmail.com>
2025-10-27 20:12:09 -0600

    Get guitars2

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ const createCart = (carrito) => {
                                 <tr>
                                     <th>Imagen</th>
                                     <th>Nombre</th>
-                                    <th>Precio</th>
+                                    <th>precio</th>
                                     <th>Cantidad</th>
                                     <th></th>
                                 </tr>
